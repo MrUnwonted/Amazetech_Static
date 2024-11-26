afc01c471038b95bb66e91cc675a88aac4d0dd99
MrUnwonted <arjungodzarjun@gmail.com>
2024-11-26 15:40:28 +0530

    Added Folders

---
+++ src/routes/index.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { BrowserRouter as Router,  Routes } from 'react-router-dom';
+
+const AppRoutes = () => (
+    <Router>
+      <Routes>
+        {/* <Route path="/" element={< />} /> */}
+        {/* <Route path="/stakeholders" element={<StakeholderPage />} />
+        <Route path="/risks" element={<RiskIssueActionPage />} />
+        <Route path="/benefits" element={<BenefitRegister />} />
+        <Route path="/project-plan" element={<ProjectPlanPage />} /> */}
+      </Routes>
+    </Router>
+  );
+  
+  export default AppRoutes;
